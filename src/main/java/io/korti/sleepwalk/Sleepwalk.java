54be8caa336e37f862bcdb1b7f17722c39825c4a
Korti <lucas.korthauer@gmail.com>
2020-08-12 15:42:59 +0200

    Added basic minecraft forge project.

---
+++ src/main/java/io/korti/sleepwalk/Sleepwalk.java
@@ -0,0 +1,34 @@
+/*
+ *  Copyright 2020 Korti
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package io.korti.sleepwalk;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(Sleepwalk.MOD_ID)
+public class Sleepwalk {
+
+    public static final String MOD_ID = "sleepwalk";
+    public static final Logger LOG = LogManager.getLogger();
+
+    public static Sleepwalk instance;
+
+    public Sleepwalk() {
+        Sleepwalk.instance = this;
+    }
+}
